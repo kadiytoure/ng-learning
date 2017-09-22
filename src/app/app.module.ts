ea29505079344a789599600e3a93dae8aea685d3
toure <tourekadiy@gmail.com>
2017-09-22 10:37:41 +0200

    boucle.component.ts & app.module.ts;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TemplateComponent } from './template/template.component';
 import { BoucleComponent } from './boucle/boucle.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoService } from './shared/todo.service';
+import { TodoSecondComponent } from './todo-second/todo-second.component';
+import { CompteurService } from './shared/compteur/compteur.service';
+import { TodoAjaxService } from './shared/todo-ajax.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateComponent,
-    BoucleComponent
+    BoucleComponent,
+    TodoComponent,
+    TodoSecondComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     TodoService,
