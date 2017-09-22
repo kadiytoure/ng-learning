1f7069f1d8277bf5758b479ac93d718b545a5c40
toure <tourekadiy@gmail.com>
2017-09-22 12:07:56 +0200

    exercice;

--- src/app/shared/todo-ajax.service.ts
+++ src/app/shared/todo-ajax.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import 'rxjs/add/operator/toPromise';
+import { Todo } from "./todo";
 
 
 
@@ -9,7 +9,18 @@ export class TodoAjaxService {
     'http://localhost:3000/todo';
     constructor(private http:HttpClient){}
 
-    getAllTodo() {
-        return this.http.get(this.urlAPI).toPromise();
+    getAllTodo():Promise<Todo[]> {
+        return this.http.get<Todo[]>(this.urlAPI).toPromise();
+    }
+
+    addTodo(todo:Todo):Promise<Todo> {
+    return this.http.post<Todo>(this.urlAPI, todo)
+    .toPromise();
+    }
+
+    removeTodo(todo:Todo):Promise<any> {
+        return this.http.delete(this.urlAPI+'/'+todo.id)
+        .toPromise();
+        
     }
 }
--- src/app/shared/todo.component.ts
+++ src/app/shared/todo.component.ts
@@ -1,1 +1,38 @@
-import { TodoAjaxService } from
+import { Component, OnInit } from '@angular/core';
+
+import { TodoAjaxService } from '../shared/todo-ajax.service';
+
+
+@Component({
+    selector: 'app-todo',
+    templateUrl: '.todo.component.html',
+    styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit{
+    liste:Object[];
+    newTodo:string;
+}
+
+constructor(private todoService:TodoAjaxService){
+}
+
+ngOnInit() {
+    this.todoService.getAllTodo().then((todos) =>
+this.liste = <object[]>todos);
+}
+
+addTodo(){
+    /*
+    Le addTodo attend un objet de type Todo, donc un 
+    objet possédant au moins une propriété message et 
+    une propriété id;
+    */
+    this.todoService.addTodo({message:this.newTodo})
+    .then((todo) => this.liste.push(todo));
+}
+
+removeTodo(index:number){
+this.todoService.removeTodo({id:index, message:''})
+.then(()=> this.liste = this.liste.filter((todo)
+=> todo.id !== index));
+}
